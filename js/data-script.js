69dadb1aa97b0e1f394d08dcf0d08fdc4e38e86d
adinoegroho <37955862+adinoegroho@users.noreply.github.com>
2018-05-07 00:49:23 +0700

    Add files via upload

--- js/data-script.js
+++ js/data-script.js
@@ -1,104 +1,26 @@
 $('#submitBtn').click(function()
 {
+	// Deklarasi variabel untuk input value, label value, input status, kondisi pengisian
 	var inputFields = [];
-	var inputFilled = [];
 	var labelForId = [];
 	var indexInput = 0;
 
+	// Mendapatkan semua input form yg ada pada halaman html
 	$('input').each(function()
 	{
-		inputFields[indexInput] = $(this).attr('id');
-		indexInput++;
-	});
-
-	indexInput = 0;
-	$('label.labelInput').each(function()
-	{
-		labelForId[indexInput] = $(this).text();
-		indexInput++;
-	});
-
-	console.log("Panjang Array : inputFields = " + inputFields.length + " dan inputFilled = " + inputFilled.length);
-	
-	for(var num = 0; num < inputFields.length; num++)
-	{
-		console.log(inputFields[num] + " dengan label " + labelForId[num] + " : " + inputFilled[num]);
-	}
-
-	for(var indexNum = 0; indexNum < inputFields.length; indexNum++)
-	{
-		inputId = $(inputFields[indexNum]).attr('id');
-
-		if($(inputFields[indexNum]).val() == "" || $(inputFields[indexNum]).val() == 0 || $(inputFields[indexNum]).val() == null)
+		// Mengecek input form telah terisi atau tidak
+		if($(this).val() != "" && $(this).val() != undefined && $(this).val() != null && $(this).val() != 0)
 		{
-			var filledCondition;
-			for(var num = 0; num < inputFields.length; num++)
-			{
-				// Mendapatkan nilai true/false pada tiap form input yang terisi/tidak
-				if ($("input." + inputId).val() == "")
-				{
-					inputFilled[num] = false;
-				}
-				else
-				{
-					inputFilled[num] = true;
-				}
-
-				// Membandingkan semua form input dengan kondisi '&&'(Tiap kondisi harus bernilai benar)
-				if (num == 1)
-				{
-					filledCondition = inputFilled[num];
-				}
-				else
-				{
-					filledCondition = inputFilled[num] && inputFilled[num-1];
-				}
-			}
-
-			if(!filledCondition)
-			{
-				$('div#notifSubmition').empty();
-				$('#notifSubmition').append('<div class="notifSubmition col-sm-12"><h4 class="text-center">' + labelForId[indexNum] + ' tidak boleh kosong.</h4></div>');
-				$('div.notifSubmition').addClass('notifSubmition notifFailed');
-
-				return false;
-			}
+			// Menambahkan komponen didalam halaman html (Untuk menampilkan kondisi setelah melakukan Submit)
+			$('div#notifSubmition').empty();
+			$('#notifSubmition').append('<div class="notifSubmition col-sm-12"><h4 class="text-center">Data berhasil disimpan.</h4></div>');
+			$('div.notifSubmition').addClass('notifSubmition notifSuccess');
 		}
-		else if ($(inputFields[indexNum]).val() != "" || $(inputFields[indexNum]).val() != 0 || $(inputFields[indexNum]).val() != null)
+		else
 		{
-			var filledCondition;
-			for(var num = 0; num < inputFields.length; num++)
-			{
-				// Mendapatkan nilai true/false pada tiap form input yang terisi/tidak
-				if ($("input." + inputId).val() == "")
-				{
-					inputFilled[num] = false;
-				}
-				else
-				{
-					inputFilled[num] = true;
-				}
-
-				// Membandingkan semua form input dengan kondisi '&&'(Tiap kondisi harus bernilai benar)
-				if (num == 1)
-				{
-					filledCondition = inputFilled[num];
-				}
-				else
-				{
-					filledCondition = inputFilled[num] && inputFilled[num-1];
-				}
-			}
-
-			if(filledCondition)
-			{
-				$('div#notifSubmition').empty();
-				$('#notifSubmition').append('<div class="notifSubmition col-sm-12"><h4 class="text-center">Data berhasil disimpan.</h4></div>');
-				$('div.notifSubmition').addClass('notifSubmition notifSuccess');
-
-				return false;
-			}
-		}	
-	}
-
+			$('div#notifSubmition').empty();
+			$('#notifSubmition').append('<div class="notifSubmition col-sm-12"><h4 class="text-center">Semua Form harus tersisi.</h4></div>');
+			$('div.notifSubmition').addClass('notifSubmition notifFailed');
+		}
+	});
 });
